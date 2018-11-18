5fde3aee1bed3f8355dc93d10e7a2234638cbf49
Wedge <shadowpilot34@gmail.com>
2018-11-18 15:17:59 +0300

    Change from coffee to js

---
+++ app/assets/javascripts/active_admin/select2/select2.js
@@ -0,0 +1,20 @@
+$(function() {
+  'use strict';
+  let initSelect2 = function(inputs, extra) {
+    let = extra || {}
+    inputs.each(function() {
+      item = $(this)
+      options = $.extend({ allowClear: true }, extra, item.data('select2'))
+      item.data('select2', null)
+      item.select2(options)
+    })
+  }
+
+  $(document).on('has_many_add:after', '.has_many_container', function(e, fieldset) {
+    initSelect2(fieldset.find('.select2-input'))
+  })
+
+  $(function() {
+    initSelect2($(".select2-input"), { placeholder: "" })
+  })
+})
